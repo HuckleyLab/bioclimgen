9c3ef16d0c5ff8380b66e9f8a14f4d4d843d5dce
Tony Cannistra <tony.cannistra@gmail.com>
2017-05-10 11:48:14 -0700

    generalized db ingest

--- db_ingest.sh
+++ db_ingest.sh
@@ -1,6 +1,6 @@
-psql -d climate -f db_ingest.sql
+psql -d climate -f /Users/tony/Dropbox/Projects/UW/bioclimgen/db_ingest.sql
 
-for file in climate/1911-1920/temp/*.csv
+for file in *.csv
 	psql -d climate -c "\copy bioclim_geom(index, lat, lon, year, bio1, bio2, bio3, bio4, bio5, bio6, bio7, bio8, bio9, bio10, bio11, bio12, bio13, bio14, bio15, bio16, bio17, bio18, bio19) from '$file' delimiter ',' csv header"
 end
 
